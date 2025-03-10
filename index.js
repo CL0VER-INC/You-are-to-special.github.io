fd805c2d0e854fd5a64ee4d6cead96c90b8a8b5d
CL0VER-INC <cloverincorporation01@gmail.com>
2025-03-09 22:43:01 -0600

    index.js

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ const botonAtras = document.querySelector('.controles button.atras');
 const botonAdelante = document.querySelector('.controles button.adelante');
 
 const canciones = [
-    { titulo: 'Qué bonito fue', nombre: 'El David', fuente: 'Musica/Qué bonito fue.mp3' },
-    { titulo: 'Una Mañana', nombre: 'José José', fuente: 'Musica/Una Mañana.mp3' },
-    { titulo: 'I Love You So', nombre: 'The Walters', fuente: 'Musica/I Love You So.mp3' },
-    { titulo: 'En Otra Vida', nombre: 'Yami Safdie; Lasso', fuente: 'Musica/En Otra Vida.mp3' },
-    { titulo: 'R U Mine?', nombre: 'Arctic Monkeys', fuente: 'Musica/R U Mine.mp3' },
-    { titulo: 'Es Por Ti', nombre: 'Juanes', fuente: 'Musica/Es Por Ti.mp3' },
-    { titulo: 'Shut up My Moms Calling', nombre: 'Hotel Ugly', fuente: 'Musica/Shut up My Moms Calling.mp3' },
-    { titulo: 'Snowman', nombre: 'Sia', fuente: 'Musica/Snowman.mp3' },
-    { titulo: 'Eres', nombre: 'Café Tacvba', fuente: 'Musica/Eres.mp3' },
-    { titulo: 'Riptide', nombre: 'Vance Joy', fuente: 'Musica/Riptide.mp3' },
-    { titulo: 'Mr. Loverman', nombre: 'Ricky Montgomery', fuente: 'Musica/Mr. Loverman.mp3' },
-    { titulo: 'I Ran (So Far Away)', nombre: 'A Flock Of Seagulls', fuente: 'Musica/I Ran (So Far Away).mp3' },
-    { titulo: 'Nice To Meet You', nombre: 'Myles Smith', fuente: 'Musica/Nice To Meet You.mp3' },
-    { titulo: 'Out of My League', nombre: 'Fitz & The Tantrums', fuente: 'Musica/Out of My League.mp3' },
-    { titulo: 'Valentine', nombre: 'Laufey', fuente: 'Musica/Valentine.mp3' },
-    { titulo: 'I Like Me Better', nombre: 'Lauv', fuente: 'Musica/I Like Me Better.mp3' },
-    { titulo: 'Aint No Mountain High Enough', nombre: 'Marvin Gaye; Tammi Terrell', fuente: 'Musica/Aint No Mountain High Enough.mp3' }
+    { titulo: 'Qué bonito fue', nombre: 'El David', fuente: 'Qué bonito fue.mp3' },
+    { titulo: 'Una Mañana', nombre: 'José José', fuente: 'Una Mañana.mp3' },
+    { titulo: 'I Love You So', nombre: 'The Walters', fuente: 'I Love You So.mp3' },
+    { titulo: 'En Otra Vida', nombre: 'Yami Safdie; Lasso', fuente: 'En Otra Vida.mp3' },
+    { titulo: 'R U Mine?', nombre: 'Arctic Monkeys', fuente: 'R U Mine.mp3' },
+    { titulo: 'Es Por Ti', nombre: 'Juanes', fuente: 'Es Por Ti.mp3' },
+    { titulo: 'Shut up My Moms Calling', nombre: 'Hotel Ugly', fuente: 'Shut up My Moms Calling.mp3' },
+    { titulo: 'Snowman', nombre: 'Sia', fuente: 'Snowman.mp3' },
+    { titulo: 'Eres', nombre: 'Café Tacvba', fuente: 'Eres.mp3' },
+    { titulo: 'Riptide', nombre: 'Vance Joy', fuente: 'Riptide.mp3' },
+    { titulo: 'Mr. Loverman', nombre: 'Ricky Montgomery', fuente: 'Mr. Loverman.mp3' },
+    { titulo: 'I Ran (So Far Away)', nombre: 'A Flock Of Seagulls', fuente: 'I Ran (So Far Away).mp3' },
+    { titulo: 'Nice To Meet You', nombre: 'Myles Smith', fuente: 'Nice To Meet You.mp3' },
+    { titulo: 'Out of My League', nombre: 'Fitz & The Tantrums', fuente: 'Out of My League.mp3' },
+    { titulo: 'Valentine', nombre: 'Laufey', fuente: 'Valentine.mp3' },
+    { titulo: 'I Like Me Better', nombre: 'Lauv', fuente: 'I Like Me Better.mp3' },
+    { titulo: 'Aint No Mountain High Enough', nombre: 'Marvin Gaye; Tammi Terrell', fuente: 'Aint No Mountain High Enough.mp3' }
 ];
 
 let indiceCancionActual = 0;
